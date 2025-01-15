6ea0118fdfcab4bfbc9268fc753cef7362148fa9
jquie <jacquelynphan03@gmail.com>
2025-01-15 18:22:20 +0000

    not done

--- Main.java
+++ Main.java
@@ -5,15 +5,19 @@ public class Main
   public static void main(String[] args)
   {
     Scanner sc = new Scanner (System.in);
-    System.out.println("Write two strings:")
+    System.out.println("Write two strings:");
     String sentence = sc.nextLine();
     String target = sc.nextLine();
     int index = sentence.indexOf(target);
-    if (!(index = -1)
+    if (index != 0)
     {
       String answer = sentence.substring(0, index) + sentence.substring(index + target.length());
+      System.out.println(answer);
     }
-    System.out.println(answer);
+    else
+    {
+      System.out.println("Substring not found.");
+    }
   }
 
   public static int countSub(String word, String target)
